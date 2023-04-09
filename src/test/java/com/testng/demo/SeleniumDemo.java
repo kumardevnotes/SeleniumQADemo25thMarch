8e412088bbbe5e189dcc2f3291b923881463d79c
kumardevnotes <kumar.thoudishetti@gmail.com>
2023-04-09 22:03:24 +0530

    added

---
+++ src/test/java/com/testng/demo/SeleniumDemo.java
@@ -0,0 +1,74 @@
+package com.testng.demo;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.testng.annotations.Test;
+
+public class SeleniumDemo {
+
+	@Test (priority=2)
+	public void launchAppInChrome() throws Exception {
+		
+		System.out.println("Launching the app in Chrome");
+		
+		//To Get root folder 
+		String rootFolder  = System.getProperty("user.dir");
+		
+		//to set the path of the Chrome driver file
+		System.setProperty("webdriver.chrome.driver", rootFolder+"//src//test//resources//chromedriver.exe");
+		
+		//to launch browser
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--remote-allow-origins=*");
+		ChromeDriver driver = new ChromeDriver(options);
+	
+		
+		//to maximize the browser
+		driver.manage().window().maximize();
+		
+		//to launch app in the browser
+		driver.get("https://facebook.com");
+		
+		//to wait for 3 seconds
+		Thread.sleep(3000);
+		
+		//to close the browser
+		driver.quit();
+		
+		System.out.println("App closed which is opened in Chrome");
+	}
+
+	@Test (priority=1)
+	public void launchAppInEdge() throws Exception {
+
+		System.out.println("Launching the app in Edge");
+		
+		//To Get root folder 
+		String rootFolder  = System.getProperty("user.dir");
+		
+		//to set the path of the Chrome driver file
+		System.setProperty("webdriver.edge.driver", rootFolder+"//src//test//resources//msedgedriver.exe");
+		
+		//to launch browser
+		EdgeOptions  options = new EdgeOptions ();
+		options.addArguments("--remote-allow-origins=*");
+		EdgeDriver driver = new EdgeDriver(options);
+	
+		
+		//to maximize the browser
+		driver.manage().window().maximize();
+		
+		//to launch app in the browser
+		driver.get("https://gmail.com");
+		
+		//to wait for 3 seconds
+		Thread.sleep(3000);
+		
+		//to close the browser
+		driver.quit();
+		
+		System.out.println("App closed which is opened in Chrome");
+	}
+}
---
+++ src/test/java/com/testng/demo/SeleniumDemo2.java
@@ -0,0 +1,76 @@
+package com.testng.demo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class SeleniumDemo2 {
+
+	@Parameters({ "browser" })
+	@Test
+	public void verifyUserLogin(String browser) throws Exception {
+		String rootFolder  = System.getProperty("user.dir");
+		WebDriver driver = null ;
+		
+		switch(browser) {
+		
+		case "Chrome":
+		System.setProperty("webdriver.chrome.driver", rootFolder+"//src//test//resources//chromedriver.exe");
+		ChromeOptions chromeOptions = new ChromeOptions();
+		chromeOptions.addArguments("--remote-allow-origins=*");
+		driver = new ChromeDriver(chromeOptions);
+		break;
+		
+		case "Edge":
+	    System.setProperty("webdriver.edge.driver", rootFolder+"//src//test//resources//msedgedriver.exe");
+		//to launch browser
+		EdgeOptions  edgeOptions = new EdgeOptions ();
+		edgeOptions.addArguments("--remote-allow-origins=*");
+		driver = new EdgeDriver(edgeOptions);
+		break;
+		
+		default:
+			Assert.fail("Please pass valid browser name as parameter");
+		}
+
+		driver.manage().window().maximize();
+		
+		//Speak Languages — Learn a new language online
+		//Launching app
+		driver.get("https://speaklanguages.com");
+		Thread.sleep(2000);
+		
+		//clicking on login link using selenium methods
+		//driver.findElement(By.id("nav_login")).click(); //id is called selenium locator
+		
+		driver.findElement(By.linkText("Log in")).click(); //linkText is called selenium locator
+		
+		//entering email address and passwords into text fields on login page
+		driver.findElement(By.id("email_input")).sendKeys("johnnitesh2@gmail.com");
+		driver.findElement(By.id("password_input")).sendKeys("Testing@123");
+		
+		//clicking on login button
+		driver.findElement(By.id("login_button")).click();
+		Thread.sleep(4000);
+		
+		String loggedInUser = driver.findElement(By.id("nav_user")).getText();
+		System.out.println(loggedInUser);
+		
+		if(loggedInUser.equals("nitesh")) {
+			System.out.println("Login is success");
+		}
+		else {
+			Assert.fail("Login is failed. Please check the logins");
+		}
+		
+		driver.quit();
+		
+	}
+
+}
