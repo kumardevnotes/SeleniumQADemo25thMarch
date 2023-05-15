cfc1526b7a74bbb6bbaf1b4171406e8127eb9cde
kumardevcg <thoudishetti-kumar.thoudishetti-kumar@capgemini.com>
2023-05-15 11:46:56 +0530

    added new file and updated one for file for demo

---
+++ src/test/java/TestDemo1.java
@@ -0,0 +1,9 @@
+
+public class TestDemo1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello All");
+
+	}
+
+}
--- src/test/java/com/arrays/demo/ArraysDemo2.java
+++ src/test/java/com/arrays/demo/ArraysDemo2.java
@@ -18,6 +18,8 @@ public class ArraysDemo2 {
 		for(int i=0; i < myNumbers.length ; i ++) {
 			System.out.println(myNumbers[i]);
 		}
+		
+		System.out.println("Hello Arrays");
 	}
 
 }
