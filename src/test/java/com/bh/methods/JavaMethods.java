71c2a16cf51edf68562960b45c5823f89fa9b65c
kumardevnotes <kumar.thoudishetti@gmail.com>
2023-05-15 15:30:10 +0530

    added new changes

---
+++ src/test/java/com/bh/methods/JavaMethods.java
@@ -0,0 +1,38 @@
+package com.bh.methods;
+
+public class JavaMethods {
+
+	// Java method is a block of statements
+	public static void main(String[] args) {
+		String empName = "John Miller";
+		//System.out.println(empName);
+
+		// calling a method by its name in main()
+		//displayEmpSalay();
+		
+		displayEmpPincode();
+		displayName("Kumar");
+
+		displayEmpPincode();
+		displayName("Mehatha");
+
+		displayEmpPincode();
+		displayName("Saloni");
+	}
+
+	// Declaring a static method in java
+	public static void displayEmpSalay() {
+		System.out.println("100000");
+	}
+
+	// Declaring a static method in java
+	public static void displayEmpPincode() {
+		System.out.println("500072");
+	}
+
+	// Declaring a static method with String parameter
+	public static void displayName(String empName) {
+		System.out.println("empName given is " + empName);
+	}
+
+}
